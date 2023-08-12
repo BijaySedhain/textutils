77180507c43299e6fcb0c1959aacbeb724a6c9e4
Bijay Sedhain <bijaysedhain@gmail.com>
2023-08-12 08:17:33 -0500

    Added Text summary and Convert to lowerCase

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -8,6 +8,11 @@ export default function TextForm(props) {
     let newText = text.toUpperCase();
     setText(newText);
   }
+  const handleLowClick = () => {
+    // console.log("UpperCase was Clicked "+ text)
+    let newText = text.toLowerCase();
+    setText(newText);
+  }
   const handleOnChange = (event) => {
     setText(event.target.value);
     // console.log("OnChange")
@@ -17,12 +22,22 @@ const [text, setText] = useState('Enter Text Here');
 
 // setText("New Text");
   return (
-    <div>
+    <>
+    <div className="container">
         <h1>{props.heading}</h1>
         <div className="mb-3">
             <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
         </div>  
-        <button className="btn btn-primary" onClick={handleUpClick}>Conver to UpperCase</button>
+        <button className="btn btn-primary mx-1" onClick={handleUpClick}>Conver to UpperCase</button>
+        <button className="btn btn-primary mx-1" onClick={handleLowClick}>Conver to LowerCase</button>
     </div>
+    <div className="containter my-2">
+      <h1>Your Text Summary</h1>
+      <p>{text.split(" ").length-1} words and {text.length} characters</p>
+      <p>{0.008* (text.split(" ").length)} Minutes to read</p>
+      <h2>Preview</h2>
+      <p>{text}</p>
+    </div>
+    </>
   )
 }
